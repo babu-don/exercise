082b231dc0d6c44b885fdb3ec2cc374404d1b39a
vai-bhav-github <109478639+vai-bhav-github@users.noreply.github.com>
2022-07-20 14:59:35 +0530

    Update faceprep1.cpp

--- 4.2 Advanced Pattern Questions/practice/faceprep1.cpp
+++ 4.2 Advanced Pattern Questions/practice/faceprep1.cpp
@@ -4,22 +4,26 @@ int main()
 {
    int n; cin>>n;
 
-   for(int i=0; i<=n-1; i++){
+   for(int i=0; i < n; i++){
     cout<<"* ";
     for(int j=1; j<=i; j++){
 
         cout<<j<<" ";
+        if(j == i)
+            cout<<"*";
+
     }
-    cout<<"* ";
     cout<<endl;
    }
-   for(int i=n-1; i>=0; i--){
+   for(int i=n-2; i>=0; i--){
     cout<<"* ";
     for(int j=1; j<=i; j++){
         
         cout<<j<<" ";
+        if(i==j)
+            cout<<"* ";
     }
-    cout<<"* ";
+    
     cout<<endl;
    }
    return 0;
